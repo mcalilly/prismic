c967a82ded5cfd75c2e70fb14acbfdf1de333c06
Lee McAlilly <leemcalilly@gmail.com>
2020-06-23 11:29:23 -0500

    Install tailwind css with starter templates and fix a few more minor issues with the Rails 6 upgrade

---
+++ app/javascript/css/tailwind.config.js
@@ -0,0 +1,183 @@
+// Required inside postcss.config.js
+
+module.exports = {
+  important: true,
+  theme: {
+
+    container: {
+      center: true,
+      padding: '0.75rem'
+    },
+
+    fontFamily: {
+      sans: [
+        'Roboto Regular',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif'
+      ],
+
+      serif: [
+        'Merriweather Bold',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif'
+      ]
+    },
+
+    extend: {
+      colors: {
+        black: '#000000',
+      },
+
+      fontFamily: {
+        display: [
+          'GT America Compressed Light',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        displayItalic: [
+          'GT America Compressed Light Italic',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        heading: [
+          'GT America Compressed Bold',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        headingItalic: [
+          'GT America Compressed Bold Italic',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        subheading: [
+          'Roboto Regular',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        subheadingItalic: [
+          'Roboto Italic',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        caption: [
+          'Roboto Mono Regular',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        body: [
+          'Merriweather Regular',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        bodyBold: [
+          'Merriweather Bold',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        bodyBoldItalic: [
+          'Merriweather Bold Italic',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+
+        bodyItalic: [
+          'Merriweather Regular Italic',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ]
+      },
+    }
+  },
+
+  plugins: [
+    require('@tailwindcss/ui'),
+  ]
+
+}
